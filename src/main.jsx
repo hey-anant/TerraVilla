f631e20f8f1b6662956e423f12af11331a436e4a
hey-anant <anantkumar.r77@gmail.com>
2026-06-22 18:40:36 +0530

    Fix duplicate createRoot in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-<<<<<<< HEAD
-createRoot(document.getElementById('root')).render(<StrictMode>
-  <App />
-</StrictMode>);
-=======
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
->>>>>>> 5d73c961b8b00a97ccf6a031ed7db878f06aa342
